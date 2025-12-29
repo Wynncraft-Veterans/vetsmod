5deafa8ca21449adc3ef60005a1118d35ff793f7
wenweia <202365934+wenweia@users.noreply.github.com>
2025-12-28 21:48:16 -0400

    Create project stub.

---
+++ src/client/kotlin/org/wynnvets/VetsmodClient.kt
@@ -0,0 +1,9 @@
+package org.wynnvets
+
+import net.fabricmc.api.ClientModInitializer
+
+object VetsmodClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
