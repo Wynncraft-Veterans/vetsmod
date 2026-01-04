cd082c40a02800c54e6d892a73b6879b6f7edfc6
wenweia <202365934+wenweia@users.noreply.github.com>
2026-01-04 17:52:13 -0400

    Split client.

---
+++ src/client/java/org/wynnvets/VetsmodClient.java
@@ -0,0 +1,10 @@
+package org.wynnvets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VetsmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
