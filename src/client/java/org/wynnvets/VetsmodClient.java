933282d83e5b34035cb268a4b02a32862fb274be
wenweia <202365934+wenweia@users.noreply.github.com>
2026-01-04 18:22:47 -0400

    Implement placeholder command.

--- src/client/java/org/wynnvets/VetsmodClient.java
+++ src/client/java/org/wynnvets/VetsmodClient.java
@@ -1,10 +1,19 @@
 package org.wynnvets;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.minecraft.network.chat.Component;
 
 public class VetsmodClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// Register client-side commands using the Fabric client command API
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("motd").executes(ctx -> {
+				ctx.getSource().sendFeedback(Component.literal("hello world"));
+				return 1;
+			}));
+		});
 	}
 }
